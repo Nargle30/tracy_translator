8160db9f521317a0dd9ff4befbca27c5384f7d7a
Daria Kozlova <nargle30@gmail.com>
2022-01-16 19:58:57 +0100

    make the bot run and answer

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
 console.log('Hello world!')
 // run npm run start:dev to start dev
 // more info: https://khalilstemmler.com/blogs/typescript/node-starter-project/
+
+// const Reverso = require('reverso-api')
+
+import dotenv from 'dotenv'
+import {App} from '@slack/bolt'
+
+dotenv.config()
+const env = process.env
+// const reverso = new Reverso()
+
+const app = new App({
+  token: env.TOKEN,
+  signingSecret: env.SIGNING_SECRET,
+  socketMode: true,
+  appToken: env.APP_TOKEN,
+});
+
+app.start(3000).then(() => {
+  console.log('⚡️ Bolt app is running!')
+})
+
+
+app.message('Hi', async ({message, say}) => {
+
+  await say(`_Hello, I am Tracy_`);
+});
+
+app.event('app_home_opened', async ({event, say}) => {
+  await say('Oh hello there!')
+})
